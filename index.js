a2eb10ffa011a4567a4e8075300717cb15e61a4e
reedk10 <40337728+reedk10@users.noreply.github.com>
2018-09-11 18:20:49 -0400

    Exercise 2

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-const lis = $('li');
+$(document).ready(function () {
 
-$(document).ready (function () {
-  function main() {
+  $(".menu").hide();
+  $(".hamburger").click(function () {
+    $(".menu").slideToggle("slow", function () {
+    });
 
-   $('#grow-me').addClass('big');
+    $('#numerals').on('click', function () {
+      $(".letters").hide();
+      $(".numbers").show();
+    })
 
-   $('#shrink-me').removeClass('big');
+    $('#alpha').on('click', function () {
+      $(".numbers").hide();
+      $(".letters").show();
+    })
+  });
 
-  $( "li" ).text(function( index ) {
-  console.log($(this).text() );
-   });
-
-   $('#hide-me').css('display', 'none');
-   $('#show-me').css('display', 'block');
-   
-   $('a').attr('href', 'https://www.example.com').text('somewhere');
-
-   $('#name')
-     .keyup(function() {
-       var value = $(this).val();
-       $("h1").text("Welcome " + value);
-     })
-     .keyup();
-
-   
-    
-  }
-
-
-  $("#action").on("click", main);
 });
-
-
-
